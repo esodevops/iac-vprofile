b6e8db4e1966a23b0a4fdd8ccf63cbd765b0a57d
kalpeshmali90 <kalpeshmali90@gmail.com>
2025-12-20 12:43:23 +0530

    staging workflow

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,13 +27,14 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofileactions200400"
     key    = "terraform.tfstate"
     region = "us-east-2"
   }
 
   required_version = "~> 1.6.3"
 }
+##
 ##
 ##
 ##
